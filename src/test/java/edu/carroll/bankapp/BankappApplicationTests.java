6d1749463e3d024485a3c497b8f93bce0577ccf6
Justin Forseth <jforseth210@gmail.com>
2023-09-01 10:41:15 -0600

    Initial commit

---
+++ src/test/java/edu/carroll/bankapp/BankappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carroll.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8e12ff48d03e58d58ccc1c93186c6326752653c7
Justin Forseth <jforseth210@gmail.com>
2023-09-08 11:35:25 -0600

    Control accounts from the backend

--- src/main/java/edu/carroll/bankapp/web/controller/DashboardController.java
+++ src/main/java/edu/carroll/bankapp/web/controller/DashboardController.java
@@ -1,12 +1,25 @@
 package edu.carroll.bankapp.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class DashboardController {
+    private String[] accounts = new String[]{"Checking", "Savings", "Credit", "Cash"};
+
     @GetMapping("/")
-    public String index() {
+    public RedirectView index() {
+        return new RedirectView("/" + accounts[0]);
+    }
+
+    @GetMapping("/{account}")
+    public String index(@PathVariable String account, Model model) {
+        System.out.println(account);
+        model.addAttribute("accounts", accounts);
+        model.addAttribute("currentAccount", account);
         return "index";
     }
 }

bb018e39306cd39ed7afd4835b38c8ccb3d5fcd4
Abby Brown <amb0330@icloud.com>
2023-10-02 11:14:45 -0600

    Javadoc added to Account.java

--- src/main/java/edu/carroll/bankapp/jpa/model/Account.java
+++ src/main/java/edu/carroll/bankapp/jpa/model/Account.java
@@ -26,46 +26,101 @@ public class Account {
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
+    /**
+     * Returns the name of the account
+     *
+     * @return name - String - the name of the account
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the account
+     *
+     * @param name - String - the given name of the account
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the ID of the account
+     *
+     * @return id - auto-incrementing Integer - the id of the account
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the account
+     *
+     * @param id - Integer - the id of the account
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Returns the user associated with the account (who 'owns' the account)
+     *
+     * @return owner - User - the owner of the account
+     */
     public User getOwner() {
-        return owner;
+        return this.owner;
     }
 
+    /**
+     * Sets the user associated with the account (the 'owner' of the account)
+     *
+     * @param owner - User - the owner of the account
+     */
     public void setOwner(User owner) {
         this.owner = owner;
     }
 
+    /**
+     * Gets a list of all the transactions
+     *
+     * @return transactions - Set<> - list of transactions
+     */
     public Set<Transaction> getTransactions() {
-        return transactions;
+        return this.transactions;
     }
 
+    /**
+     * Sets a list of transactions
+     *
+     * @param transactions - Set<> - list of transactions
+     */
     public void setTransactions(Set<Transaction> transactions) {
         this.transactions = transactions;
     }
 
+    /**
+     * Gets the account balance in cents (there should not be floating point values)
+     *
+     * @return balanceInCents - Integer - the account balance in cents
+     */
     public Integer getBalanceInCents() {
-        return balanceInCents;
+        return this.balanceInCents;
     }
 
+    /**
+     * Returns the account balance in dollars using simple math
+     *
+     * @return double - returns account balance in dollars (double)
+     */
     public double getBalanceInDollars() {
         return (double) balanceInCents / (double) 100;
     }
 
+    /**
+     * Sets the account balance in cents
+     *
+     * @param balanceInCents - Integer - account balance in cents
+     */
     public void setBalanceInCents(Integer balanceInCents) {
         this.balanceInCents = balanceInCents;
     }

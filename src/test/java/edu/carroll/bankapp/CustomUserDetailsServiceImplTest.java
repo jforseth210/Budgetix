0a2fc5871541981e97919ca45468685153c50361
Justin Forseth <jforseth210@gmail.com>
2023-11-21 19:31:00 -0700

    Rewrite test data population

--- src/test/java/edu/carroll/bankapp/CustomUserDetailsServiceImplTest.java
+++ src/test/java/edu/carroll/bankapp/CustomUserDetailsServiceImplTest.java
@@ -1,7 +1,7 @@
 package edu.carroll.bankapp;
 
 import edu.carroll.bankapp.service.CustomUserDetailsService;
-import edu.carroll.bankapp.testdata.TestUsers;
+import edu.carroll.bankapp.service.UserService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,30 +20,55 @@ public class CustomUserDetailsServiceImplTest {
     private CustomUserDetailsService customUserDetailsService;
 
     @Autowired
-    private TestUsers testUsers;
+    private UserService userService;
+
+    public static final String JOHN_NAME = "John Doe";
+    public static final String JOHN_EMAIL = "john@example.com";
+    public static final String JOHN_USERNAME = "johndoe";
+    public static final String JOHN_PASSWORD = "password123";
+
+    public static final String JANE_NAME = "Jane Smith";
+    public static final String JANE_EMAIL = "jane@example.com";
+    public static final String JANE_USERNAME = "janesmith";
+    public static final String JANE_PASSWORD = "letmein456";
+
+    public static final String ALICE_NAME = "Alice Johnson";
+    public static final String ALICE_EMAIL = "alice@example.com";
+    public static final String ALICE_USERNAME = "alicejohnson";
+    public static final String ALICE_PASSWORD = "mysecret123";
+
+    public static final String UNICODE_NAME = "𝔘𝔫𝔦𝔠𝔬𝔡𝔢 𝔐𝔞𝔫!";
+    public static final String UNICODE_EMAIL = "iliketobreakthings@email.com";
+    public static final String UNICODE_USERNAME = "☕☕☕☕☕";
+    public static final String UNICODE_PASSWORD = "⿈⍺✋⇏⮊⎏⇪⤸Ⲥ↴⍁➄⼉⦕ⶓ∧⻟⍀⇝⧽";
+
+    public static final String BAD_USER_NAME = "Bob Marley";
+    public static final String BAD_USER_EMAIL = "bobby";
+    public static final String BAD_USER_USERNAME = "B";
+    public static final String BAD_USER_PASSWORD = "p";
 
     @Test
     public void testLoadUserByUsernameValidUser() {
         // Create user
-        testUsers.createJohnDoe();
+        userService.createUser(JOHN_NAME, JOHN_EMAIL, JOHN_USERNAME, JOHN_PASSWORD);
         // Create some other users
-        testUsers.createAliceJohnson();
-        testUsers.createJaneSmith();
+        userService.createUser(ALICE_NAME, ALICE_EMAIL, ALICE_USERNAME, ALICE_PASSWORD);
+        userService.createUser(JANE_NAME, JANE_EMAIL, JANE_USERNAME, JANE_PASSWORD);
 
         // Fetch user
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(TestUsers.JOHN_USERNAME);
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(JOHN_USERNAME);
 
         // Validate user
         assertNotNull(userDetails);
-        assertEquals(TestUsers.JOHN_USERNAME, userDetails.getUsername());
+        assertEquals(JOHN_USERNAME, userDetails.getUsername());
     }
 
     @Test
     public void testLoadUserByUsernameInvalidUser() {
         // Create some users
-        testUsers.createJohnDoe();
-        testUsers.createAliceJohnson();
-        testUsers.createJaneSmith();
+        userService.createUser(JOHN_NAME, JOHN_EMAIL, JOHN_USERNAME, JOHN_PASSWORD);
+        userService.createUser(ALICE_NAME, ALICE_EMAIL, ALICE_USERNAME, ALICE_PASSWORD);
+        userService.createUser(JANE_NAME, JANE_EMAIL, JANE_USERNAME, JANE_PASSWORD);
 
         // Make sure not existent user throws error
         assertThrows(UsernameNotFoundException.class, () -> {
@@ -55,17 +80,18 @@ public class CustomUserDetailsServiceImplTest {
     @Test
     public void testLoadUserByUsernameUnicodeUsername() {
         // Create user
-        testUsers.createUnicodeMan();
+        userService.createUser(UNICODE_NAME, UNICODE_EMAIL, UNICODE_USERNAME, UNICODE_PASSWORD);
+        ;
         // Create some other users
-        testUsers.createJohnDoe();
-        testUsers.createAliceJohnson();
-        testUsers.createJaneSmith();
+        userService.createUser(JOHN_NAME, JOHN_EMAIL, JOHN_USERNAME, JOHN_PASSWORD);
+        userService.createUser(ALICE_NAME, ALICE_EMAIL, ALICE_USERNAME, ALICE_PASSWORD);
+        userService.createUser(JANE_NAME, JANE_EMAIL, JANE_USERNAME, JANE_PASSWORD);
 
         // Fetch user
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(TestUsers.UNICODE_USERNAME);
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(UNICODE_USERNAME);
 
         // Make sure unicode strings match
         assertNotNull(userDetails);
-        assertEquals(TestUsers.UNICODE_USERNAME, userDetails.getUsername());
+        assertEquals(UNICODE_USERNAME, userDetails.getUsername());
     }
 }
